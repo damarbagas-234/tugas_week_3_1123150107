25e32b6450f9a7bb899cf2e4007c8faa748d9849
damarbagas-234 <muhammaddamar107@gmail.com>
2025-10-03 17:19:09 +0700

    deskripsi perubahan

--- bin/classmahasiswa.dart
+++ bin/classmahasiswa.dart
@@ -3,12 +3,14 @@ class Mahasiswa {
   String nama;
   String nim;
   String jurusan;
+  String alamat;
   int angkatan;
   // Constructor
   Mahasiswa({
     required this.nama,
     required this.nim,
     required this.jurusan,
+    required this.alamat,
     required this.angkatan,
   });
   // Method
@@ -16,6 +18,7 @@ class Mahasiswa {
     print("Nama : $nama");
     print("NIM : $nim");
     print("Jurusan : $jurusan");
+    print("Alamat : $alamat");
     print("Angkatan : $angkatan");
   }
 }
@@ -27,7 +30,33 @@ void main(List<String> args) {
     nim: '1123150107',
     jurusan: 'Informatika',
     angkatan: 2023,
+    alamat: 'JL.Sukabakti No.23',
   );
 
   mhs.tampilkanData();
+}
+class AsistenDosen extends Mahasiswa {
+  String mataKuliah;
+
+  AsistenDosen({
+    required String nama,
+    required String nim,
+    required String jurusan,
+    required String alamat,
+    required int angkatan,
+    required this.mataKuliah,
+  }) : super(
+          nama: nama,
+          nim: nim,
+          jurusan: jurusan,
+          alamat: alamat,
+          angkatan: angkatan,
+        );
+
+  // Override method
+  @override
+  void tampilkanData() {
+    super.tampilkanData();
+    print("Asisten MK: $mataKuliah");
+  }
 }
