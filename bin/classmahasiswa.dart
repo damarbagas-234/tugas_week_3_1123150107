beee939e32a1558243f11ffa4efafc3a00edca0c
damarbagas-234 <muhammaddamar107@gmail.com>
2025-10-03 16:46:49 +0700

    ngumpulin tugas

---
+++ bin/classmahasiswa.dart
@@ -0,0 +1,33 @@
+class Mahasiswa {
+  // Properties (atribut)
+  String nama;
+  String nim;
+  String jurusan;
+  int angkatan;
+  // Constructor
+  Mahasiswa({
+    required this.nama,
+    required this.nim,
+    required this.jurusan,
+    required this.angkatan,
+  });
+  // Method
+  void tampilkanData() {
+    print("Nama : $nama");
+    print("NIM : $nim");
+    print("Jurusan : $jurusan");
+    print("Angkatan : $angkatan");
+  }
+}
+
+void main(List<String> args) {
+  // Contoh penggunaan kelas Mahasiswa
+  final mhs = Mahasiswa(
+    nama: 'Muhamad Damar Bagas',
+    nim: '1123150107',
+    jurusan: 'Informatika',
+    angkatan: 2023,
+  );
+
+  mhs.tampilkanData();
+}
