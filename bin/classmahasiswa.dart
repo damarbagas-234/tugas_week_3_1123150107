df243cc811f99b0c7ff2a29a5939ab75f3ac4e5c
damarbagas-234 <muhammaddamar107@gmail.com>
2025-10-03 17:41:32 +0700

    deskripsi perubahan

--- bin/classmahasiswa.dart
+++ bin/classmahasiswa.dart
@@ -23,18 +23,8 @@ class Mahasiswa {
   }
 }
 
-void main(List<String> args) {
-  // Contoh penggunaan kelas Mahasiswa
-  final mhs = Mahasiswa(
-    nama: 'Muhamad Damar Bagas',
-    nim: '1123150107',
-    jurusan: 'Informatika',
-    angkatan: 2023,
-    alamat: 'JL.Sukabakti No.23',
-  );
+// ...existing code...
 
-  mhs.tampilkanData();
-}
 class AsistenDosen extends Mahasiswa {
   String mataKuliah;
 
@@ -46,12 +36,12 @@ class AsistenDosen extends Mahasiswa {
     required int angkatan,
     required this.mataKuliah,
   }) : super(
-          nama: nama,
-          nim: nim,
-          jurusan: jurusan,
-          alamat: alamat,
-          angkatan: angkatan,
-        );
+         nama: nama,
+         nim: nim,
+         jurusan: jurusan,
+         alamat: alamat,
+         angkatan: angkatan,
+       );
 
   // Override method
   @override
@@ -59,4 +49,68 @@ class AsistenDosen extends Mahasiswa {
     super.tampilkanData();
     print("Asisten MK: $mataKuliah");
   }
+}
+
+// Interface for pendaftaran (registration)
+abstract class Pendaftaran {
+  void daftarMatkul(String matkul);
+}
+
+class MahasiswaAktif extends Mahasiswa implements Pendaftaran {
+  List<String> matkulDiambil = [];
+  MahasiswaAktif({
+    required String nama,
+    required String nim,
+    required String jurusan,
+    required String alamat,
+    required int angkatan,
+  }) : super(
+         nama: nama,
+         nim: nim,
+         jurusan: jurusan,
+         alamat: alamat,
+         angkatan: angkatan,
+       );
+
+  @override
+  void daftarMatkul(String matkul) {
+    matkulDiambil.add(matkul);
+    print("$nama berhasil daftar mata kuliah $matkul");
+  }
+}
+
+void main() {
+  // Buat object mahasiswa biasa
+  var mhs1 = Mahasiswa(
+    nama: "Muhamad Damar Bagas",
+    nim: "1123150107",
+    jurusan: "Teknik Informatika",
+    angkatan: 2023,
+    alamat: "Jl. Sukabakti No.23",
+  );
+  mhs1.tampilkanData();
+  print("----------");
+  // Buat object asisten dosen
+  var asdos = AsistenDosen(
+    nama: "Ketut Gunawan",
+    nim: "1224536789",
+    jurusan: "Tenik Informatika",
+    angkatan: 2023,
+    alamat: "Jl. Sukabakti No.23",
+    mataKuliah: "Pemrograman Mobile",
+  );
+  asdos.tampilkanData();
+  print("----------");
+  // Mahasiswa aktif implementasi interface
+  var mhs2 = MahasiswaAktif(
+    nama: "Muhamad Damar Bagas",
+    nim: "1123150107",
+    jurusan: "Teknik Informatika",
+    angkatan: 2023,
+    alamat: "Jl. Sukabkati No.23",
+  );
+  mhs2.daftarMatkul("Algoritma");
+  mhs2.daftarMatkul("Basis Data");
+  mhs2.tampilkanData();
+  print("Matkul yang diambil: ${mhs2.matkulDiambil}");
 }
