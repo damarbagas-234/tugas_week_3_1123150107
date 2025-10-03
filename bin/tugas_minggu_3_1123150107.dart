4137ea78614e9b3d861c7d22ca6450b0f5c457b0
damarbagas-234 <muhammaddamar107@gmail.com>
2025-10-03 16:12:28 +0700

    first commit

---
+++ bin/tugas_minggu_3_1123150107.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_minggu_3_1123150107/tugas_minggu_3_1123150107.dart' as tugas_minggu_3_1123150107;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_minggu_3_1123150107.calculate()}!');
+}
---
+++ test/tugas_minggu_3_1123150107_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugas_minggu_3_1123150107/tugas_minggu_3_1123150107.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
